89f0f87439c2872da548bef515c0e19e703a9f7e
coofzilla <jeric614@yahoo.com>
2024-02-03 15:22:07 +0900

    feature: work with date picker

--- SwiftUI/project4/BetterRest/BetterRest/ContentView.swift
+++ SwiftUI/project4/BetterRest/BetterRest/ContentView.swift
@@ -9,12 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     @State var sleepAmount = 8.0
+    @State var datePicker = Date()
 
     var body: some View {
         VStack {
-            Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, step: 0.25)
+            Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 0.25 ... 12, step: 0.25)
+            DatePicker("Please enter a date", selection: $datePicker)
+                .labelsHidden()
         }
         .padding()
+    }
+
+    func exampleDates() {
+        let now = Date()
+        let tomorrow = now.addingTimeInterval(TimeInterval(86400))
+        let range = now ... tomorrow
     }
 }
 
